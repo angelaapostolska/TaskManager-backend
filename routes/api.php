5516fa1d95769f329d2dd457cf8afa4e6645750d
AngelaApostolska <160138124+angelaapostolska@users.noreply.github.com>
2025-04-18 12:58:19 +0200

    implemented: auth

--- routes/api.php
+++ routes/api.php
@@ -2,20 +2,27 @@
 
 use App\Actions\DeleteTask;
 use App\Actions\MarkCompleted;
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\TaskController;
 use App\MarkPending;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+//public route: login
+Route::post('/login', [AuthController::class, 'login']);
 
+//protected routes
+Route::middleware('auth:sanctum')->group(function () {
 
-Route::apiResource('tasks', TaskController::class);
-Route::patch('/tasks/{task}/completeTask', [MarkCompleted::class, 'completeTask']);
-Route::patch('/tasks/{task}/deleteTask', [DeleteTask::class, 'deleteTask']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 
+    //logout route
+    Route::post('/logout', [AuthController::class, 'logout']);
 
-
-
+    //Task related routes
+    Route::apiResource('tasks', TaskController::class);
+    Route::patch('/tasks/{task}/completeTask', [MarkCompleted::class, 'handle']);
+    Route::patch('/tasks/{task}/deleteTask', [DeleteTask::class, 'handle']);
+});
